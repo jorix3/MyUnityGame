f4f474a73c338f51ef8b835217f4e3dc124b105c
Jorix3 <jyri.virtaranta@cs.tamk.fi>
2017-06-06 10:27:34 +0300

    asd

---
+++ sanic/SANic/Assets/scriptsts/kvsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kvsScript : MonoBehaviour {
+
+    public float speed;
+    private bool goingRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (goingRight) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        goingRight = !goingRight;
+    }
+}
