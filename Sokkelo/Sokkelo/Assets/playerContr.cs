ee934a0edcdaa9758871935a86508bbbc2740896
Jorix3 <jyri.virtaranta@cs.tamk.fi>
2017-06-07 13:10:08 +0300

    "valmis"

---
+++ Sokkelo/Sokkelo/Assets/playerContr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerContr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Respawn") {
+            SceneManager.LoadScene("scene1");
+        }
+        if (other.tag == "Finish") {
+            SceneManager.LoadScene("scene2");
+        }
+    }
+}
