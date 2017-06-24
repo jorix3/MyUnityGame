8e1469ff71f6592c4611badfac0a5289d54f5257
jorix3 <jyri.virtaranta@cs.tamk.fi>
2017-06-25 01:06:37 +0300

    added musac

---
+++ ESsimuator/ES simulator/Assets/player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    public Text countText;
+    public Text winText;
+    private int count;
+    private Rigidbody rb;
+    public AudioClip audio1;
+    public AudioClip audio2;
+    private AudioSource source;
+    private float volume;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        volume = 0.1f;
+        CountUpdate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ES")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            CountUpdate();
+
+            if (count < 12)
+            {
+                source.PlayOneShot(audio1, volume);
+            }
+            else
+            {
+                source.PlayOneShot(audio2, volume);
+            }
+        }
+    }
+
+    void CountUpdate()
+    {
+        countText.text = "ES tölkkejä: " + count.ToString() + "/12";
+        volume += 0.1f;
+
+        if (volume > 1)
+        {
+            volume = 1f;
+        }
+
+        if (count >= 12)
+        {
+            winText.text = " Voitit pelin!";
+        }
+        else
+        {
+            winText.text = "";
+        }
+    }
+}
